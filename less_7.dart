7768655d5d9584b234e544df5c4f38981d80e3ef
Господин <rdogdurbaev1@gmail.com>
2022-11-29 20:05:46 +0600

    first commit

---
+++ less_7.dart
@@ -0,0 +1,92 @@
+void main() {
+  // int i = 1;
+
+  // while (i <= 5) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // do {
+  //   print(i);
+  //   i++;
+  // } while (i <= 20);
+
+  // while (i <= 20) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // for (var i = 0; i <= 100; i += 2) {
+  //   print(i);
+  // }
+
+  // #1 zadanie
+
+  // for (var i = 1; i <= 100; i++) {
+  //   print(i);
+  // }
+
+  // int i = 1;
+
+  // while (i <= 100) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // #2 zadanie
+
+  // for (var i = 11; i <= 33; i++) {
+  //   print(i);
+  // }
+
+  // int b = 11;
+  // while (b <= 33) {
+  //   print(b);
+  //   b++;
+  // }
+
+  // #3 zadanie
+
+  // for (var i = 0; i <= 100; i += 2) {
+  //   print(i);
+  // }
+
+  // int c = 2;
+  // while (c <= 100) {
+  //   if (c % 2 == 0) {
+  //     print(c);
+  //   }
+  //   c++;
+  // }
+
+  // #4 zadanie
+
+  // for (var i = 0; i < 100; i++) {
+  //   if (i){
+
+  //   }
+  // }
+
+  // int d = 2;
+  // while (d<= 100) {
+  //   if (d  2) {
+  //     print(d);
+  //   }
+  //   d++;
+  // }
+
+  // int sum = 0;
+
+  // for (var i = 0; i <= 100; i++) {
+  //   sum += i;
+
+  // }
+
+  // print(sum);
+
+  // while (i <= 100) {
+  //   sum += i;
+  //   i++;
+  // }
+  // print(sum);
+}
